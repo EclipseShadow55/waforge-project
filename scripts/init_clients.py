5fb0508afc4bd10bd2981d62ba63dd3f1e0af7a4
EShadow5 <141190316+EShadow5@users.noreply.github.com>
2025-02-15 19:36:48 -0800

    Commit

--- scripts/init_clients.py
+++ scripts/init_clients.py
@@ -1,9 +1,19 @@
 import json
-import googlemaps
+from google import genai
+
+
+class Auth:
+    def __init__(self, key = None, secret = None, client = None):
+        self.key = key
+        self.secret = secret
+        self.client = client
+
 
 def get_clients():
-    with open("data.json") as f:
+    with open("creds.json") as f:
         data = json.load(f)
-    gmap = googlemaps.Client(data["google"])
+    gemini = Auth(client=(genai.Client(data["gemini"])))
+    amadeus = Auth(key=data["amadeus"][0], secret=data["amadeus"][1])
 
-    return {"gmap": gmap}
+    return {"gemini": gemini,
+            "amadeus": amadeus}
--- scripts/main.py
+++ scripts/main.py
@@ -4,19 +4,20 @@ import init_clients
 clients = init_clients.get_clients()
 
 
-def get_data(origin: str,
-             descriptors: str,
-             departure_date: str,
-             return_date: str,
-             adults: str,
-             children: str,
-             infants: str,
-             trav_class: str,
-             airlines: str,
-             non_stop: str,
-             max_price: str):
+def main(origin_airport: str,
+         descriptors: str,
+         departure_date: str,
+         return_date: str,
+         adults: str,
+         children: str,
+         infants: str,
+         trav_class: str,
+         non_stop: str,
+         max_price: str):
     try:
         pass
+        # Get location (airport) from gemini based on origin_airport, descriptors, and max_price
+        # Get flight data using amadeus
     except Exception as e:
         return None
 
